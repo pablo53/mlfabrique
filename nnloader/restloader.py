96d79567b98544e903516eef0c9b58ba7c054cad
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-02-22 11:01:18 +0100

    MongDB config for nnloader.

--- nnloader/restloader.py
+++ nnloader/restloader.py
@@ -1,9 +1,20 @@
+import os
 from flask import Flask
 from flask import json as fjson
 from flask import request
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
+mongouri = os.getenv('MONGO_URI')
+if mongouri == None:
+    mongohost = os.getenv('MONGO_HOST')
+    mongoport = os.getenv('MONGO_PORT')
+    if mongohost != None and mongoport != None:
+        mongouri = "mongodb://{}:{}/".format(mongohost, mongoport)
+    else:
+        mongouri = "mongodb://localhost:27017/"
+print("Using MongoDB URI: {}".format(mongouri), flush=True)
 
 
 @app.route('/ping', methods=['GET'])
